61fc5b6275a1c0fe550693a8abf0787a164fb3f7
DavidSimma <simma.david@gmail.com>
2021-12-09 13:57:08 +0100

    Fertig

---
+++ Firma_Vererbung/Programm.py
@@ -0,0 +1,103 @@
+if __name__ == "__main__":
+    class Person:
+        def __init__(self, firstname, lastname, age, isMale):
+            self.firstname = firstname
+            self.lastname = lastname
+            self.age = age
+            self.isMale = isMale
+
+
+    class Employee(Person):
+        def __init__(self, firstname, lastname, age, gender, salary):
+            super().__init__(firstname, lastname, age, gender)
+            self.salray = salary
+
+
+    class Leader(Employee):
+        def __init__(self, firstname, lastname, age, gender, salary):
+            super().__init__(firstname, lastname, age, gender, salary)
+
+
+    class Department:
+        def __init__(self, name):
+            self.name = name
+
+        leader = Leader("", "", 0, True, 0)
+        employees = []
+
+
+    class Company:
+        departments = []
+
+    
+    e1 = Employee("Sebi", "Rimml", 19, False, 1800)
+    e2 = Employee("Daniel", "Niederhauser", 18, True, 2400)
+    l1 = Leader("David", "Simma", 18, True, 3200)
+    d1 = Department("IT")
+    d1.leader = l1
+    d1.employees.append([e1, e2])
+    c1 = Company()
+    c1.departments.append([d1])
+
+
+    def countDeps(com=Company()):
+        return com.departments.count()
+
+
+    def countEmp(com=Company()):
+        count = 0
+        for d in com.departments:
+            for e in d:
+                for a in e.employees:
+                    count += len(a)
+        return count
+
+
+    def countLead(com=Company()):
+        count = 0
+        for d in com.departments:
+            for e in d:
+                if e.leader is not None:
+                    count += 1
+        return count
+
+
+    def biggestDep(com=Company()):
+        count = 0
+        bigCount = 0
+        dep = None
+        for d in com.departments:
+            for e in d:
+                if e.leader is not None:
+                    count += 1
+                for a in e.employees:
+                    count += len(a)
+                if bigCount < count:
+                    bigCount = count
+                    dep = e
+                count = 0
+        return dep.name
+
+
+    def howManyMen(com=Company()):
+        ges = 0
+        count = 0
+        for d in com.departments:
+            for e in d:
+                if e.leader is not None:
+                    if e.leader.isMale:
+                        count += 1
+                    ges += 1
+                for a in e.employees:
+                    for b in a:
+                        if b.isMale:
+                            count += 1
+                        ges += 1
+
+        return count / ges * 100
+
+
+    print(countEmp(c1))
+    print(countLead(c1))
+    print(biggestDep(c1))
+    print(howManyMen(c1), "%")
